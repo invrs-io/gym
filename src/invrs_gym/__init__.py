eb2a44a2b4422780960b30294ff17b945b2c9baa
Martin Schubert <30735893+mfschubert@users.noreply.github.com>
2023-11-02 11:27:02 -0700

    Explicitly cast dimensionless quantities as int (#49)
    * Explicitly cast dimensionless quantities as int
    
    * Version updated from v0.2.0 to v0.2.1

--- src/invrs_gym/__init__.py
+++ src/invrs_gym/__init__.py
@@ -3,7 +3,7 @@
 Copyright (c) 2023 The INVRS-IO authors.
 """
 
-__version__ = "v0.2.0"
+__version__ = "v0.2.1"
 __author__ = "Martin F. Schubert <mfschubert@gmail.com>"
 
 from invrs_gym import challenges as challenges
