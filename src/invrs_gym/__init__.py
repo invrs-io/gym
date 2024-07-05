6f00a26ce061a756507bca307c3ec9184d593c51
Martin Schubert <mfschubert@gmail.com>
2024-07-05 12:54:17 -0700

    Version updated from v0.11.0 to v1.0.0

--- src/invrs_gym/__init__.py
+++ src/invrs_gym/__init__.py
@@ -3,7 +3,7 @@
 Copyright (c) 2023 The INVRS-IO authors.
 """
 
-__version__ = "v0.11.0"
+__version__ = "v1.0.0"
 __author__ = "Martin F. Schubert <mfschubert@gmail.com>"
 
 from invrs_gym import challenges as challenges
