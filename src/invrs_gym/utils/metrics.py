a3446b8f694380db40f820178cc660a5963016d7
Martin Schubert <mfschubert@gmail.com>
2024-07-08 13:54:08 -0700

    Allow grayscale interface pixels

--- src/invrs_gym/utils/metrics.py
+++ src/invrs_gym/utils/metrics.py
@@ -6,6 +6,7 @@ Copyright (c) 2023 The INVRS-IO authors.
 from typing import Any, Optional
 
 import jax.numpy as jnp
+from jax import lax
 from jax import tree_util
 from totypes import types
 
@@ -37,10 +38,47 @@ def binarization_degree(params: PyTree) -> Optional[jnp.ndarray]:
 
     def _compute_degree(d: types.Density2DArray) -> jnp.ndarray:
         array = (d.array - d.lower_bound) / (d.upper_bound - d.lower_bound)
-        violation_lo = jnp.abs(array)
-        violation_hi = jnp.abs(1 - array)
-        violation = jnp.minimum(violation_lo, violation_hi)
-        return 1 - 2 * jnp.amax(violation)
+        return _array_binarization_degree(array)
 
     degrees = [_compute_degree(d) for d in densities]
     return jnp.amax(jnp.asarray(degrees))
+
+
+def _array_binarization_degree(x: jnp.ndarray) -> jnp.ndarray:
+    """Compute the binarization degree for an array.
+
+    Intermediate-valued pixels at the interface between solid and void regions
+    are ignored.
+
+    Args:
+        x: The array for which the binarization degree is sought.
+
+    Returns:
+        The scalar binarization degree.
+    """
+    x = x.reshape((-1,) + x.shape[-2:])
+
+    violation_lo = jnp.abs(x)
+    violation_hi = jnp.abs(1 - x)
+    violation = jnp.minimum(violation_lo, violation_hi)
+
+    # Interface pixels must be surrounded by at least four solid or void pixels.
+    solid_or_void_pixels = (violation == 0).astype(float)
+    solid_or_void_pixels = jnp.pad(
+        solid_or_void_pixels,
+        ((0, 0), (1, 1), (1, 1)),
+        constant_values=1,
+    )
+    neighbors = lax.conv_general_dilated(
+        lhs=solid_or_void_pixels[jnp.newaxis, ...],
+        rhs=jnp.ones((1, 1, 3, 3)),
+        padding="VALID",
+        dimension_numbers=("NCHW", "OIHW", "NCHW"),
+        window_strides=(1, 1),
+    )
+    neighbors = jnp.squeeze(neighbors, axis=(0, 1))
+    interface_pixels = neighbors >= 4
+
+    violation = jnp.where(interface_pixels, 0.0, violation)
+    degree = 1 - 2 * jnp.amax(violation)
+    return degree
--- tests/utils/test_metrics.py
+++ tests/utils/test_metrics.py
@@ -9,7 +9,7 @@ import jax.numpy as jnp
 from parameterized import parameterized
 from totypes import types
 
-from invrs_gym.utils import metrics
+from invrs_gym.utils import metrics, transforms
 
 
 class BinarizationDegreeTest(unittest.TestCase):
@@ -34,13 +34,84 @@ class BinarizationDegreeTest(unittest.TestCase):
         ]
     )
     def test_binarization_degree_matches_expected(self, array, expected, scale):
+        array = jnp.kron(jnp.asarray(array), jnp.ones((5, 5)))
         self.assertEqual(
             metrics.binarization_degree(
                 types.Density2DArray(
-                    array=jnp.asarray(array) * scale,
+                    array=array * scale,
                     lower_bound=-scale,
                     upper_bound=scale,
                 ),
             ),
             expected,
         )
+
+    @parameterized.expand(
+        [
+            [0, 0],
+            [1, 0],
+            [2, 0],
+            [10, 0],
+            [0, 0],
+            [1, 1],
+            [2, 2],
+            [10, 10],
+        ]
+    )
+    def test_binarization_for_downsampled_binary_circle(self, xshift, yshift):
+        x, y = jnp.meshgrid(jnp.arange(-50, 50), jnp.arange(-50, 50))
+        circle = (((x - xshift) ** 2 + (y - yshift) ** 2) < 35**2).astype(float)
+        circle = transforms.box_downsample(circle, (20, 20))
+        circle_density = types.Density2DArray(
+            array=circle,
+            lower_bound=0,
+            upper_bound=1,
+        )
+        self.assertEqual(metrics.binarization_degree(circle_density), 1.0)
+
+    @parameterized.expand(
+        [
+            [0, 0],
+            [1, 0],
+            [2, 0],
+            [10, 0],
+            [0, 0],
+            [1, 1],
+            [2, 2],
+            [10, 10],
+        ]
+    )
+    def test_binarization_for_downsampled_binary_square(self, xshift, yshift):
+        x, y = jnp.meshgrid(jnp.arange(-50, 50), jnp.arange(-50, 50))
+        square = ((x - xshift) ** 2 < 35**2) & ((y - yshift) ** 2 < 35**2)
+        square = square.astype(float)
+        square = transforms.box_downsample(square, (20, 20))
+        square_density = types.Density2DArray(
+            array=square,
+            lower_bound=0,
+            upper_bound=1,
+        )
+        self.assertEqual(metrics.binarization_degree(square_density), 1.0)
+
+    @parameterized.expand(
+        [
+            [0, 0],
+            [1, 0],
+            [2, 0],
+            [10, 0],
+            [0, 0],
+            [1, 1],
+            [2, 2],
+            [10, 10],
+        ]
+    )
+    def test_binarization_for_downsampled_triangle(self, xshift, yshift):
+        x, y = jnp.meshgrid(jnp.arange(-50, 50), jnp.arange(-50, 50))
+        triangle = x + y > 0
+        triangle = transforms.box_downsample(triangle, (20, 20))
+        triangle_density = types.Density2DArray(
+            array=triangle,
+            lower_bound=0,
+            upper_bound=1,
+        )
+        self.assertEqual(metrics.binarization_degree(triangle_density), 1.0)
